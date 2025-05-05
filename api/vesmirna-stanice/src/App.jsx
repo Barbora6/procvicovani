d43edff903370f17f921dde545bc9c9ab8978493
Barbora6 <smetanovabarbora@email.cz>
2025-05-05 23:32:08 +0200

    Projekt: Vesmírná stanice

---
+++ api/vesmirna-stanice/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Projekty API</h1>
+      <ul>
+        <li>
+          <Link to="/vesmirna-stanice">Vesmírná stanice</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ api/vesmirna-stanice/src/components/VesmirnaStanice.jsx
@@ -0,0 +1,3 @@
+export const VesmirnaStanice = () => {
+  return <div></div>;
+};
