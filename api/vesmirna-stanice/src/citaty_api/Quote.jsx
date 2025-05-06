a1569636d731bb19117ccf0af4f48a678faf270f
Barbora6 <smetanovabarbora@email.cz>
2025-05-06 10:36:01 +0200

    oprava překlepu

--- api/vesmirna-stanice/src/citaty_api/Quote.jsx
+++ api/vesmirna-stanice/src/citaty_api/Quote.jsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+
 export const Quote = () => {
-  return <div></div>;
+  const [quote, setQuote] = useState("");
+
+  const getQuote = async () => {
+    const response = await fetch("https://api.kanye.rest/");
+    const data = await response.json();
+    setQuote(data["quote"]);
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  return (
+    <div>
+      <h1>Citáty</h1>
+      <h3>{quote}</h3>
+      <button onClick={() => getQuote()}>Další citát</button>
+    </div>
+  );
 };
